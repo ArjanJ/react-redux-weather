8224c67c0a65eab1194e90bf89625b45503a58ee
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-04-06 22:57:24 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path 								= require('path');
+const webpack							= require('webpack');
+const htmlWebpackPlugin 	= require('html-webpack-plugin');
+
+module.exports = {
+	devtool: 'cheap-eval-source-map',
+	entry: [
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/dev-server',
+		'./src/app/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new htmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+	],
+	module: {
+		preLoaders: [{
+			test: /\.js$/,
+			loaders: ['eslint-loader'],
+			exclude: /node_modules/
+		}],
+		loaders: [{
+			test: /\.js$/,
+			loaders: ['babel'],
+			exclude: /node_modules/
+		}]
+	}
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+};
